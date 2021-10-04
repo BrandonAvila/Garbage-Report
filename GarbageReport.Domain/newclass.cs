18594e7678330f80c89ba2e34e81753a6c9cf6ff
Brandon Ávila <20090419@alumno.utmetropolitana.edu.mx>
2021-10-04 10:26:27 -0500

    Archivos agregados

---
+++ GarbageReport.Domain/newclass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GarbageReport.Domain
+{
+    public class newclass
+    {
+        
+    }
+}
